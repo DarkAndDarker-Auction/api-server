89f6882d84b43dc50dc64cba011b0a2840f32ff1
Envyw6567 <nbw970508@gmail.com>
2023-09-18 21:13:22 +0900

    #1 feat : jwt인증 기능 구현

--- src/main/java/com/darkanddarker/auction/common/jwt/TokenDto.java
+++ src/main/java/com/darkanddarker/auction/common/jwt/TokenDto.java
@@ -1,2 +1,18 @@
-package com.darkanddarker.auction.common.jwt;public class TokenDto {
+package com.darkanddarker.auction.common.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiresIn;
+    private String authority;
 }
