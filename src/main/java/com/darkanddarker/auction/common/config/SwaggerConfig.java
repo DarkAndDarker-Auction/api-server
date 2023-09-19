e095fb59840d8bcb7f1a2173e6290e2f924ea697
Envyw6567 <nbw970508@gmail.com>
2023-09-19 14:10:03 +0900

    chore : gradle 수정 및 Swagger 적용

--- src/main/java/com/darkanddarker/auction/common/config/SwaggerConfig.java
+++ src/main/java/com/darkanddarker/auction/common/config/SwaggerConfig.java
@@ -1,2 +1,25 @@
-package com.darkanddarker.auction.common.config;public class SwaggerConfig {
+package com.darkanddarker.auction.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-DarkAndDarker-Auction")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("DarkAndDarker Auction API")
+                        .description("DarkAndDarker Third-Party Auction API")
+                        .version("v0.0.1"));
+    }
 }
--- src/main/java/com/darkanddarker/auction/common/exception/BadRequestException.java
+++ src/main/java/com/darkanddarker/auction/common/exception/BadRequestException.java
@@ -1,2 +1,7 @@
-package com.darkanddarker.auction.common.exception;public class BadRequestException {
+package com.darkanddarker.auction.common.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(final String message) {
+        super(message);
+    }
 }
